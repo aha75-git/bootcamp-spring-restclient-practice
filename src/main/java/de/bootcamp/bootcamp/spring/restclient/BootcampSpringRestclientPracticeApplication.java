d72201f220c2276fb902380d432282815b37de9a
lazio <ahaffner75@gmail.com>
2025-09-18 11:59:40 +0200

    First commit

---
+++ src/main/java/de/bootcamp/bootcamp/spring/restclient/BootcampSpringRestclientPracticeApplication.java
@@ -0,0 +1,13 @@
+package de.bootcamp.bootcamp.spring.restclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampSpringRestclientPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootcampSpringRestclientPracticeApplication.class, args);
+    }
+
+}
